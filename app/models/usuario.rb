0e4fec0aa8331586039f2856b3966c92165c1037
mateobelloso <mateobp97@alu.ing.unlp.edu.ar>
2020-11-13 20:27:34 -0300

    correccion registrar usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,6 +1,14 @@
 class Usuario < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :vencimiento, presence: { if: :suscripcion? } 
+  validates :nrotarjeta, uniqueness: true,
+  						 numericality: true,
+  						 length: { is: 16 },
+  						 presence: { if: :suscripcion? }
+  validates :codseguridad, numericality: true,
+  						   length: { in: 3..4 },
+  						   presence: { if: :suscripcion? }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
