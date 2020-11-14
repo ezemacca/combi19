7c7e4caa7c0692e5568286b79b67331b3506141b
Ezequiel Macca <ezequielmaccarini@gmail.com>
2020-11-14 18:13:06 -0300

    traducciones

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,2 +1,8 @@
 class Producto < ApplicationRecord
+	validates :nombre, uniqueness: { case_sensitive: false }
+	before_save :minuscula_nombre 
+	
+	def minuscula_nombre
+   	   self.nombre.downcase
+	end
 end
