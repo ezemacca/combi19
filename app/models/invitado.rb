3d6e5d1f1724e2e8773ad3ae9c629c6a1b4f18d9
mateobelloso <mateobp97@alu.ing.unlp.edu.ar>
2020-12-01 16:14:15 -0300

    uniqueness si o no?

--- app/models/invitado.rb
+++ app/models/invitado.rb
@@ -1,2 +1,3 @@
 class Invitado < ApplicationRecord
+	validates :DNI, numericality: true, length: { in: 7..9 }, uniqueness: true
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,5 @@
 class Usuario < ApplicationRecord
-  has_many :calificacions, dependent: :destroy
+  #has_many :calificacions, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   #validates :vencimiento,presence: true, if: :suscripcion 
