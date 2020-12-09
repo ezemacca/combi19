9a785da19342ebb6e2c8d17f0e1d1f44e57428c9
mateobelloso <mateobp97@alu.ing.unlp.edu.ar>
2020-12-09 19:34:27 -0300

    donde nos sentamos

--- app/mailers/usuario_mailer.rb
+++ app/mailers/usuario_mailer.rb
@@ -3,13 +3,23 @@ class UsuarioMailer < ApplicationMailer
 	
 	def confirmacion_compra
 		@pasaje1= params[:pasaje]
-    	@usuario1 =  params[:usuario]    	
+    	@usuario1 =  params[:usuario]  
+      @viaje = Viaje.find(@pasaje1.viaje_id)
+      @combi= Combi.find(@viaje.combi)
+      @ruta= Rutum.find(@viaje.ruta)
+      @origen= Lugar.find(@ruta.origen)
+      @destino= Lugar.find(@ruta.destino)
+      @invitados = @pasaje1.invitados  	
     	mail(to: @usuario1.email, subject: 'Combi-19 Notifiacion') #con (, pasaje: @pasaje1) no anda
   	end
   	def cancelarpasaje
   		@pasaje= params[:pasaje]
   		@usuario = params[:usuario]
   		@viaje = Viaje.find(@pasaje.viaje_id)
+      @combi= Combi.find(@viaje.combi)
+      @ruta= Rutum.find(@viaje.ruta)
+      @origen= Lugar.find(@ruta.origen)
+      @destino= Lugar.find(@ruta.destino)
   		mail(to: @usuario.email, subject: 'Combi-19 Cancelacion de pasaje')
   	
   	end	
